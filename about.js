8f2de56ab6cdead882361a8ee650d61126b84db0
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-03-16 19:09:45 +0500

    Create an art expo website, responsive both for mobile and desktop

--- about.js
+++ about.js
@@ -1,39 +1,38 @@
 const partnersList = [
-    {
-        id: 1,
-        imgURL: './images/partner-1.svg',
-    },
-    {
-        id: 2,
-        imgURL: './images/partner-2.svg',
-    },
-    {
-        id: 3,
-        imgURL: './images/partner-3.svg',
-    },
-    {
-        id: 4,
-        imgURL: './images/partner-4.svg',
-    },
-    {
-        id: 5,
-        imgURL: './images/partner-5.svg',
-    },
+  {
+    id: 1,
+    imgURL: './images/partner-1.svg',
+  },
+  {
+    id: 2,
+    imgURL: './images/partner-2.svg',
+  },
+  {
+    id: 3,
+    imgURL: './images/partner-3.svg',
+  },
+  {
+    id: 4,
+    imgURL: './images/partner-4.svg',
+  },
+  {
+    id: 5,
+    imgURL: './images/partner-5.svg',
+  },
 ];
 const showPartners = () => {
-    const partnerSection = document.querySelector('.partners');
-    // const partnerSection = document.getElementById('partners');
-    console.log(partnerSection)
-    const partner = document.createElement('div');
-    console.log(partner)
-    partner.classList.add('partner');
-    partnerSection.appendChild(partner);
-    partnersList.map((each) => {
-        const partnerImg = document.createElement("img");
-        partnerImg.src = each.imgURL;
-        partner.appendChild(partnerImg);
-    });
-}
+  const partnerSection = document.querySelector('.partners');
+  // const partnerSection = document.getElementById('partners');
+  const partner = document.createElement('div');
+  partner.classList.add('partner');
+  partnerSection.appendChild(partner);
+  partnersList.map((each) => {
+    const partnerImg = document.createElement('img');
+    partnerImg.src = each.imgURL;
+    partner.appendChild(partnerImg);
+    return true;
+  });
+};
 showPartners();
 const hamburger = document.querySelector('.hamburger');
 const navbarItems = document.querySelector('.navbarItems');
@@ -67,4 +66,19 @@ navbarItems.addEventListener('click', () => {
   navbar.classList.remove('overlay');
   hamburger.classList.remove('hide');
   closeBtn.classList.add('hide');
+});
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+// Define the function to perform when the media query matches
+const doSomething = () => {
+  hamburger.classList.remove('fa-sharp', 'fa-solid', 'fa-bars');
+};
+// Call the function once to check if the media query matches initially
+if (mediaQuery.matches) {
+  doSomething();
+}
+// Add an event listener to perform the function when the media query matches
+mediaQuery.addEventListener('change', (event) => {
+  if (event.matches) {
+    doSomething();
+  }
 });
--- index.js
+++ index.js
@@ -1,107 +1,112 @@
 const artists = [
-    {
-        id : 1,
-        artistName : 'Yochai Benkler',
-        imageURL : './images/artist_01.svg',
-        artistIntro : 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-        artistDesc : 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-    },
+  {
+    id: 1,
+    artistName: 'Haider Ali',
+    imageURL: './images/artist (1).svg',
+    artistIntro: 'Haider Ali Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    artistDesc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+  },
 
-    {
-        id : 2,
-        artistName : 'Yochai Benkler',
-        imageURL : './images/artist_04.svg',
-        artistIntro : 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-        artistDesc : 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-    },
+  {
+    id: 2,
+    artistName: 'Nadia Khan',
+    imageURL: './images/artist (2).svg',
+    artistIntro: 'Nadia Khan Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    artistDesc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+  },
 
-    {
-        id : 3,
-        artistName : 'Yochai Benkler',
-        imageURL : './images/artist_01.svg',
-        artistIntro : 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-        artistDesc : 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-    },
+  {
+    id: 3,
+    artistName: 'Nabeel Ahmed',
+    imageURL: './images/artist (3).svg',
+    artistIntro: 'Nabeel Ahmed Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    artistDesc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+  },
 
-    {
-        id : 4,
-        artistName : 'Yochai Benkler',
-        imageURL : './images/artist_04.svg',
-        artistIntro : 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-        artistDesc : 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-    },
+  {
+    id: 4,
+    artistName: 'Otega Blessing',
+    imageURL: './images/artist (4).svg',
+    artistIntro: 'Otega Blessing Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    artistDesc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+  },
 
-    {
-        id : 5,
-        artistName : 'Yochai Benkler',
-        imageURL : './images/artist_01.svg',
-        artistIntro : 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-        artistDesc : 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-    },
+  {
+    id: 5,
+    artistName: 'Nouman Danish',
+    imageURL: './images/artist (5).svg',
+    artistIntro: 'Nnouman Danish Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    artistDesc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+  },
 
-    {
-        id : 6,
-        artistName : 'Yochai Benkler',
-        imageURL : './images/artist_04.svg',
-        artistIntro : 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-        artistDesc : 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
-    },
-]
-const body = document.getElementById ('body');
+  {
+    id: 6,
+    artistName: 'Muhammad Owais',
+    imageURL: './images/artist (6).svg',
+    artistIntro: 'Muhammad Owais Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    artistDesc: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+  },
+];
+const body = document.getElementById('body');
 const section = document.createElement('section');
 section.classList.add('artistSection');
-body.appendChild(section);
+body.insertBefore(section, body.children[4]);
+// body.appendChild(section);
 const titleDiv = document.createElement('div');
 titleDiv.classList.add('titleDiv');
 section.appendChild(titleDiv);
-const title = document.createElement('h4')
-title.textContent = "Featured Artists";
+const title = document.createElement('h4');
+title.textContent = 'Featured Artists';
 title.classList.add('titleArtistSection');
 titleDiv.appendChild(title);
-const emptyDiv = document.createElement ('div')
+const emptyDiv = document.createElement('div');
 emptyDiv.classList.add('borderBottom');
 titleDiv.appendChild(emptyDiv);
+const allArtists = document.createElement('div');
+allArtists.classList.add('allArtists');
+section.appendChild(allArtists);
 let i = 2;
-const showArtists = () => {
-artists.splice(0,i).map((artist)=> {
-const artistDiv = document.createElement('div');
-artistDiv.classList.add('artistDiv');
-section.appendChild(artistDiv);
-const imgDiv = document.createElement('div');
-imgDiv.classList.add('imgDiv');
-artistDiv.appendChild(imgDiv);
-const img = document.createElement("img");
-img.src = artist.imageURL;
-imgDiv.appendChild(img);
-const descDiv = document.createElement('div');
-descDiv.classList.add('descDiv');
-artistDiv.appendChild(descDiv);
-const artistName = document.createElement('h5');
-artistName.classList.add('artistName');
-artistName.textContent= artist.artistName;
-descDiv.appendChild(artistName);
-const artistIntro = document.createElement('p');
-artistIntro.classList.add('artistIntro');
-artistIntro.textContent= artist.artistIntro;
-descDiv.appendChild(artistIntro);
-const emptyDiv2 = document.createElement('div');
-emptyDiv2.classList.add('emptyDiv2');
-descDiv.appendChild(emptyDiv2);
-const artistDesc = document.createElement('p');
-artistDesc.classList.add('artistDesc');
-artistDesc.textContent= artist.artistDesc;
-descDiv.appendChild(artistDesc);
-});
-}
-showArtists();
+const showArtists = (i) => {
+  artists.splice(0, i).map((artist) => {
+    const artistDiv = document.createElement('div');
+    artistDiv.classList.add('artistDiv');
+    allArtists.appendChild(artistDiv);
+    const imgDiv = document.createElement('div');
+    imgDiv.classList.add('imgDiv');
+    artistDiv.appendChild(imgDiv);
+    const img = document.createElement('img');
+    img.src = artist.imageURL;
+    imgDiv.appendChild(img);
+    const descDiv = document.createElement('div');
+    descDiv.classList.add('descDiv');
+    artistDiv.appendChild(descDiv);
+    const artistName = document.createElement('h5');
+    artistName.classList.add('artistName');
+    artistName.textContent = artist.artistName;
+    descDiv.appendChild(artistName);
+    const artistIntro = document.createElement('p');
+    artistIntro.classList.add('artistIntro');
+    artistIntro.textContent = artist.artistIntro;
+    descDiv.appendChild(artistIntro);
+    const emptyDiv2 = document.createElement('div');
+    emptyDiv2.classList.add('emptyDiv2');
+    descDiv.appendChild(emptyDiv2);
+    const artistDesc = document.createElement('p');
+    artistDesc.classList.add('artistDesc');
+    artistDesc.textContent = artist.artistDesc;
+    descDiv.appendChild(artistDesc);
+    return true;
+  });
+};
+showArtists(i);
 const moreBtn = document.createElement('buton');
 moreBtn.classList.add('moreBtn');
 moreBtn.innerHTML = 'More';
 section.appendChild(moreBtn);
 moreBtn.addEventListener('click', () => {
-    section.removeChild(moreBtn);
-    i = artists.length;
-    showArtists();
+  section.removeChild(moreBtn);
+  i = artists.length;
+  showArtists(i);
 });
 const hamburger = document.querySelector('.hamburger');
 const navbarItems = document.querySelector('.navbarItems');
@@ -135,4 +140,22 @@ navbarItems.addEventListener('click', () => {
   navbar.classList.remove('overlay');
   hamburger.classList.remove('hide');
   closeBtn.classList.add('hide');
+});
+// Define the media query string
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+// Define the function to perform when the media query matches
+const doSomething = () => {
+  hamburger.classList.remove('fa-sharp', 'fa-solid', 'fa-bars');
+  showArtists(artists.length);
+  window.onresize = () => window.location.reload();
+};
+// Call the function once to check if the media query matches initially
+if (mediaQuery.matches) {
+  doSomething();
+}
+// Add an event listener to perform the function when the media query matches
+mediaQuery.addEventListener('change', (event) => {
+  if (event.matches) {
+    doSomething();
+  }
 });
